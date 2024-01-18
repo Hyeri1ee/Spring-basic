91a72bc0eb77231a47873bdf9ee32618c73fa3e2
Hyeri1ee <haerizian10@gmail.com>
2024-01-19 01:13:38 +0900

    git commit 1/19 ver.1

--- BoardWeb/src/main/java/polymorphism/lgTV.java
+++ BoardWeb/src/main/java/polymorphism/lgTV.java
@@ -4,4 +4,13 @@ public class lgTV {
 	public void turnOn() {
 		System.out.println("LgTV --전원 켠다.");
 	}
+	public void turnOff() {
+		System.out.println("LgTV --전원 끈다.");
+	}
+	public void soundUp() {
+		System.out.println("LgTV --소리 울린다.");
+	}
+	public void soundDown() {
+		System.out.println("LgTV --소리 내린다.");
+	}
 }
