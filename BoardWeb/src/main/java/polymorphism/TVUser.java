69ed155ac723a94a167ce553afc0ae457a8c09e9
Hyeri1ee <haerizian10@gmail.com>
2024-01-19 16:06:05 +0900

    1/19 ver.2

--- BoardWeb/src/main/java/polymorphism/TVUser.java
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -9,11 +9,10 @@ public class TVUser {
 					new GenericXmlApplicationContext("applicationContext.xml");
 		
 		//2. Spring 컨테이너로부터 필요한 객체를 요청(LookUp)한다.
-		TV tv = (TV)factory.getBean("tv");
-		tv.powerOn();
-		tv.volumeUp();
-		tv.volumeDown();
-		tv.powerOff();
+		TV tv1 = (TV)factory.getBean("tv");
+		TV tv2 = (TV)factory.getBean("tv");
+		TV tv3 = (TV)factory.getBean("tv");
+		
 		
 		//3.Sprig 컨테이너를 종료한다.
 		factory.close();
